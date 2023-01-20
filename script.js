30c2931dde3e3371d39d824a17501c077a751b3d
taisoliva <tais15oliva@gmail.com>
2023-01-19 23:22:01 -0300

    manter conectado enviar mensagem, buscar mensagem

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let nome;
 
+let dados;
+
 let dadosServidor = [];
 
-let mensagem = document.querySelector("main");
+let idSetInterval;
 
 
 perguntarNome();
@@ -14,7 +16,7 @@ function perguntarNome(){
 
 function enviaParticipante(){
 
-    const dados = {name:nome}
+    dados = {name:nome}
     console.log(dados);
     const requisicao = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",dados);
     requisicao.then(processarResposta);
@@ -41,6 +43,15 @@ function buscarMensagens(){
     promisse.then(processarMensagens);
 }
 
+function manterConexao(){
+    const promisse = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", dados);
+    promisse.then(verificaConexao)
+}
+
+function verificaConexao(){
+    console.log("estou conectado");
+}
+
 function processarMensagens(resposta){
     console.log("to aqui");
     console.log(resposta);
@@ -49,7 +60,13 @@ function processarMensagens(resposta){
 }
 
 function entrarNaSala(){
-    buscarMensagens();
+    buscarMensagens(); 
+    ativaStatus();
+    recarregaMensagens();
+}
+
+function ativaStatus(){
+    setInterval(manterConexao,5000);
 }
 
 function adicionarMensagem(){
@@ -59,24 +76,84 @@ console.log(mensagem);
 /* imprimi as mensagens e quem entrou na sala*/ 
 function imprimirDados(){
  
+    let mensagem = document.querySelector("main");
+    let aux ="";
+
     for(let i = 0; i < dadosServidor.length; i++){
         if (dadosServidor[i].type === "status"){
             
-            mensagem.innerHTML = mensagem.innerHTML + `<div class="mensagem entrou">
+            aux = aux + `<div class="mensagem entrou">
                                                        <span> (${dadosServidor[i].time}) </span>  
                                                        <span> ${dadosServidor[i].from} </span> 
                                                        <span> ${dadosServidor[i].text} </span>
-                                                    </div>`
+                                                    </div>`;
             
         }
 
        else if (dadosServidor[i].type === "message"){
-            mensagem.innerHTML = mensagem.innerHTML + `<div class="mensagem normal">
+            aux = aux + `<div class="mensagem normal">
                                                             <span> (${dadosServidor[i].time}) </span>  
                                                             <span> ${dadosServidor[i].from} </span> para 
                                                             <span> ${dadosServidor[i].to} </span>:
                                                             <span> ${dadosServidor[i].text} </span>
-                                                        </div>`
+                                                        </div>`;
+        }
+
+        else {
+            aux = aux + `<div class="mensagem reservado">
+                                                            <span> (${dadosServidor[i].time}) </span>  
+                                                            <span> ${dadosServidor[i].from} </span> reservadamente para
+                                                            <span> ${dadosServidor[i].to} </span>:
+                                                            <span> ${dadosServidor[i].text} </span>
+                                                        </div>`;
         }
     }
+
+    mensagem.innerHTML = aux;
+    
+   setTimeout(()=> document.querySelector("main div:last-child").scrollIntoView(), 0);
 }
+
+function recarregaMensagens(){
+    setInterval(buscarMensagens,3000);
+}
+
+let envio = document.querySelector("input");
+envio.addEventListener("keyup", function(event) {
+    if(event.keyCode === 13){
+        event.preventDefault();
+        enviarMensagem();
+    }
+});
+
+function enviarMensagem(){
+
+    const input = document.querySelector("input");
+    const texto = input.value; 
+    input.value = "";
+
+    if(texto === ""){
+        return;
+    }
+
+    const textoParaEnviar = {   from:nome,
+                to:'Todos',
+                text:texto,
+                type: "message"};
+
+   const requisicao = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages",textoParaEnviar);
+   
+   requisicao.then(imprimir);
+   requisicao.catch(atualizaPagina);
+}
+
+function atualizaPagina(){
+    window.location.reload();
+}
+
+function imprimir(resposta){
+    console.log("Depois de enviar");
+    buscarMensagens();
+}
+
+
